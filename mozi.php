4668b4a5fae3c185e4ff82a090305202e8d9029c
TT-RR <kaiza1mage@gmail.com>
2023-08-22 20:52:07 +0900

    文字の連結まとめ

---
+++ mozi.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Mozi</title>
+</head>
+
+<body>
+
+    <?php
+    $name = 'TT-RR';
+    // 文字列を連結するには、ドット（.）を使う。
+    echo 'こんにちは！' . $name
+
+    ?>
+
+</body>
+
+</html>
