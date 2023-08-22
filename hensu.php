ea0d02e14ba88caa6a41c50dd7c7468ede782fcb
TT-RR <kaiza1mage@gmail.com>
2023-08-22 20:41:27 +0900

    変数のまとめ

---
+++ hensu.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Hensu</title>
+</head>
+
+<body>
+
+    <!-- 「$変数名 = 値;」で変数を定義。 -->
+    <?php
+    $fruit = 'りんご';
+    echo $fruit;
+    ?>
+
+    <br>
+
+    <?php
+    $sum = 8 + 9;
+    echo $sum;
+    ?>
+
+</body>
+
+</html>
